270ba71c9f26918890a1dbbc3df69dcb1706dfd3
theadambishop7 <theadambishop@gmail.com>
2023-06-08 12:35:30 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+morse_dict = {'A': '.-',     'B': '-...',   'C': '-.-.',
+              'D': '-..',    'E': '.',      'F': '..-.',
+              'G': '--.',    'H': '....',   'I': '..',
+              'J': '.---',   'K': '-.-',    'L': '.-..',
+              'M': '--',     'N': '-.',     'O': '---',
+              'P': '.--.',   'Q': '--.-',   'R': '.-.',
+              'S': '...',    'T': '-',      'U': '..-',
+              'V': '...-',   'W': '.--',    'X': '-..-',
+              'Y': '-.--',   'Z': '--..',   ' ': '/',
+
+              '0': '-----',  '1': '.----',  '2': '..---',
+              '3': '...--',  '4': '....-',  '5': '.....',
+              '6': '-....',  '7': '--...',  '8': '---..',
+              '9': '----.'
+              }
+
+user_input = input("What phrase (letters and numbers) would you like to translate?\n").upper()
+translated_list = [morse_dict[item] for item in user_input if item in morse_dict]
+
+translated_message = " ".join(translated_list)
+print(f"Your coded message is:\n {translated_message}")
